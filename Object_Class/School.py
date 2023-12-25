2623dd4938c2eec3e0a3757c36c80ee8368a809a
Hasan-Error-Code <hasan.error.code@gmail.com>
2023-12-25 12:28:55 +0600

    class

---
+++ Object_Class/School.py
@@ -0,0 +1,15 @@
+class School:
+    name = ""
+    age = ""
+    gender = ""
+
+def Student(self):
+    Student = School()
+    Student.name = "Hasan"
+    Student.age = 23
+    Student.gender = "Male"
+def Teacher(self):
+    Teacher = School()
+    Teacher.name = "Abdul Kuddus"
+    Teacher.age = "56"
+    Teacher.gender = "Male"
---
+++ Object_Class/car2.py
@@ -0,0 +1,14 @@
+class Car:
+    name = ""
+    color = ""
+    
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+    def start(self):
+        print("Starting the Engine")
+    
+my_car = Car("Corolla", "White")
+print(my_car.name)
+print(my_car.color)
+my_car.start()
